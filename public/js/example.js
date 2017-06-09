e1431117e0b4865d8c2a197f547a869a46ed49e3
Bojan Jovanovic <bojanj6m@gmail.com>
2017-06-09 14:08:11 -0400

    ProductList and Product Component

---
+++ public/js/example.js
@@ -0,0 +1,24 @@
+React.createElement('div', {className: 'ui items'},
+  'Hello, friend! I am a basic React component.'
+)
+
+
+<div className='ui items'>
+  Hello, friend! I am a basic React component.
+</div>
+
+
+React.createElement('div', {className: 'ui items'},
+React.createElement('p', null, 'Hello, friend! I am a basic React component.')
+)
+
+
+
+<div className='ui items'>
+  <p>
+    Hello, friend! I am a basic React component.
+  </p>
+</div>
+
+
+ReactDOM.render([what], [where]);
